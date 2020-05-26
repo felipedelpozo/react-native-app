2303ec668ffbfdae9f921a52a5aa97d8f9438e7a
Felipe del Pozo <info@botspecialist.es>
2020-05-26 05:39:52 +0200

    Add absolute import and src folder

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,12 @@
  * @format
  */
 
+const path = require('path');
+const extraNodeModules = {
+  common: path.resolve(__dirname + '/../common'),
+};
+const watchFolders = [path.resolve(__dirname + '/../common')];
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
